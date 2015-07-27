f7bb46b668afce13260f5b542e55714680a32f4b
Johan Haals <johan.haals@gmail.com>
2015-07-27 12:00:13 +0200

    pile of tests

--- yopass_test.go
+++ yopass_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -21,6 +22,7 @@ func TestValidExpiration(t *testing.T) {
 type apiResponse struct {
 	Message string
 	Secret  string
+	Key     string
 }
 
 type stubNotFoundDB struct {
@@ -38,13 +40,13 @@ func TestMessageNotFoundInMemcached(t *testing.T) {
 	getHandler(response, request, new(stubNotFoundDB))
 
 	if response.Code != http.StatusNotFound {
-		t.Fatalf("Response code is %v, should be 404", response.Code)
+		t.Errorf("Response code is %v, should be 404", response.Code)
 	}
 	resp := apiResponse{}
 	json.Unmarshal(response.Body.Bytes(), &resp)
 	expected := "Secret not found"
 	if resp.Message != expected {
-		t.Fatalf("Response is %s should be '%s'", response.Body, expected)
+		t.Errorf("Response is %s should be '%s'", response.Body, expected)
 	}
 }
 
@@ -56,6 +58,10 @@ func (stubFailDB) Get(key string) (string, error) {
 	return "", errors.New("terrible failure")
 }
 
+func (stubFailDB) Set(key string, value string, expiration int32) error {
+	return errors.New("terrible failure")
+}
+
 func TestGetFailure(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/secret/73a6d946-2ee2-11e5-b8f9-0242ac110006", nil)
 	response := httptest.NewRecorder()
@@ -63,13 +69,13 @@ func TestGetFailure(t *testing.T) {
 	getHandler(response, request, new(stubFailDB))
 
 	if response.Code != http.StatusInternalServerError {
-		t.Fatalf("Response code is %v, should be 500", response.Code)
+		t.Errorf("Response code is %v, should be 500", response.Code)
 	}
 	resp := apiResponse{}
 	json.Unmarshal(response.Body.Bytes(), &resp)
 	expected := "Unable to receive secret from database"
 	if resp.Message != expected {
-		t.Fatalf("Response is %s should be '%s'", response.Body, expected)
+		t.Errorf("Response is %s should be '%s'", response.Body, expected)
 	}
 }
 
@@ -84,6 +90,10 @@ func (stubDB) Delete(key string) error {
 	return nil
 }
 
+func (stubDB) Set(key string, value string, expiration int32) error {
+	return nil
+}
+
 func TestGetSuccess(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/secret/73a6d946-2ee2-11e5-b8f9-0242ac110006", nil)
 	response := httptest.NewRecorder()
@@ -91,16 +101,90 @@ func TestGetSuccess(t *testing.T) {
 	getHandler(response, request, new(stubDB))
 
 	if response.Code != http.StatusOK {
-		t.Fatalf("Response code is %v, should be 200", response.Code)
+		t.Errorf("Response code is %v, should be 200", response.Code)
 	}
 	resp := apiResponse{}
 	json.Unmarshal(response.Body.Bytes(), &resp)
 	expected := "OK"
 	if resp.Message != expected {
-		t.Fatalf("message is %s should be '%s'", response.Body, expected)
+		t.Errorf("message is %s should be '%s'", response.Body, expected)
 	}
 	expectedSecret := `=AKJF7\sKJFVUA==`
 	if resp.Secret != expectedSecret {
-		t.Fatalf("secret is %s should be '%s'", resp.Secret, expectedSecret)
+		t.Errorf("secret is %s should be '%s'", resp.Secret, expectedSecret)
+	}
+}
+
+func TestPostSecret(t *testing.T) {
+	body := strings.NewReader(`{"expiration": 3600, "secret": "so secret"}`)
+	request, _ := http.NewRequest("POST", "/secret", body)
+	response := httptest.NewRecorder()
+	saveHandler(response, request, new(stubDB))
+
+	if response.Code != http.StatusOK {
+		t.Errorf("Response code is %v, should be 200", response.Code)
+	}
+
+	resp := apiResponse{}
+	json.Unmarshal(response.Body.Bytes(), &resp)
+	expected := "secret stored"
+	if resp.Message != expected {
+		t.Errorf("message is %s should be '%s'", response.Body, expected)
+	}
+
+}
+
+func TestInvalidExpiration(t *testing.T) {
+	body := strings.NewReader(`{"expiration": 1337, "secret": "so secret"}`)
+	request, _ := http.NewRequest("POST", "/secret", body)
+	response := httptest.NewRecorder()
+	saveHandler(response, request, new(stubDB))
+
+	if response.Code != http.StatusBadRequest {
+		t.Errorf("Response code is %v, should be 400", response.Code)
+	}
+
+	resp := apiResponse{}
+	json.Unmarshal(response.Body.Bytes(), &resp)
+	expected := "Invalid expiration specified"
+	if resp.Message != expected {
+		t.Errorf("message is %s should be '%s'", response.Body, expected)
+	}
+
+}
+
+func TestTooLongMessage(t *testing.T) {
+	body := strings.NewReader(`{"expiration": 3600, "secret": "` + strings.Join(make([]string, 12000), "x") + `"}`)
+	request, _ := http.NewRequest("POST", "/secret", body)
+	response := httptest.NewRecorder()
+	saveHandler(response, request, new(stubDB))
+
+	if response.Code != http.StatusBadRequest {
+		t.Errorf("Response code is %v, should be 400", response.Code)
+	}
+
+	resp := apiResponse{}
+	json.Unmarshal(response.Body.Bytes(), &resp)
+	expected := "Message is too long"
+	if resp.Message != expected {
+		t.Errorf("message is %s should be '%s'", response.Body, expected)
+	}
+}
+
+func TestFailToSave(t *testing.T) {
+	body := strings.NewReader(`{"expiration": 3600, "secret": "fo"}`)
+	request, _ := http.NewRequest("POST", "/secret", body)
+	response := httptest.NewRecorder()
+	saveHandler(response, request, new(stubFailDB))
+
+	if response.Code != http.StatusInternalServerError {
+		t.Errorf("Response code is %v, should be 500", response.Code)
+	}
+
+	resp := apiResponse{}
+	json.Unmarshal(response.Body.Bytes(), &resp)
+	expected := "Failed to store secret in database"
+	if resp.Message != expected {
+		t.Errorf("message is %s should be '%s'", response.Body, expected)
 	}
 }
