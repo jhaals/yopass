e6562b231d43bb4cce1ecba361d7e74885586821
Johan Haals <johan.haals@gmail.com>
2015-07-27 09:01:35 +0200

    More tests!

--- yopass_test.go
+++ yopass_test.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
-	"github.com/bradfitz/gomemcache/memcache"
 )
 
 func TestValidExpiration(t *testing.T) {
@@ -21,13 +20,47 @@ func TestValidExpiration(t *testing.T) {
 
 type apiResponse struct {
 	Message string
+	Secret  string
+}
+
+type stubNotFoundDB struct {
+	Database
+}
+
+func (stubNotFoundDB) Get(key string) (string, error) {
+	return "", errors.New("memcache: cache miss")
 }
 
 func TestMessageNotFoundInMemcached(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/secret/73a6d946-2ee2-11e5-b8f9-0242ac110006", nil)
 	response := httptest.NewRecorder()
 
-	getHandler(response, request, memcache.New("TODO"))
+	getHandler(response, request, new(stubNotFoundDB))
+
+	if response.Code != http.StatusNotFound {
+		t.Fatalf("Response code is %v, should be 404", response.Code)
+	}
+	resp := apiResponse{}
+	json.Unmarshal(response.Body.Bytes(), &resp)
+	expected := "Secret not found"
+	if resp.Message != expected {
+		t.Fatalf("Response is %s should be '%s'", response.Body, expected)
+	}
+}
+
+type stubFailDB struct {
+	Database
+}
+
+func (stubFailDB) Get(key string) (string, error) {
+	return "", errors.New("terrible failure")
+}
+
+func TestGetFailure(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/secret/73a6d946-2ee2-11e5-b8f9-0242ac110006", nil)
+	response := httptest.NewRecorder()
+
+	getHandler(response, request, new(stubFailDB))
 
 	if response.Code != http.StatusInternalServerError {
 		t.Fatalf("Response code is %v, should be 500", response.Code)
@@ -37,5 +70,37 @@ func TestMessageNotFoundInMemcached(t *testing.T) {
 	expected := "Unable to receive secret from database"
 	if resp.Message != expected {
 		t.Fatalf("Response is %s should be '%s'", response.Body, expected)
+	}
+}
+
+type stubDB struct {
+	Database
+}
+
+func (stubDB) Get(key string) (string, error) {
+	return `=AKJF7\sKJFVUA==`, nil
+}
+func (stubDB) Delete(key string) error {
+	return nil
+}
+
+func TestGetSuccess(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/secret/73a6d946-2ee2-11e5-b8f9-0242ac110006", nil)
+	response := httptest.NewRecorder()
+
+	getHandler(response, request, new(stubDB))
+
+	if response.Code != http.StatusOK {
+		t.Fatalf("Response code is %v, should be 200", response.Code)
+	}
+	resp := apiResponse{}
+	json.Unmarshal(response.Body.Bytes(), &resp)
+	expected := "OK"
+	if resp.Message != expected {
+		t.Fatalf("message is %s should be '%s'", response.Body, expected)
+	}
+	expectedSecret := `=AKJF7\sKJFVUA==`
+	if resp.Secret != expectedSecret {
+		t.Fatalf("secret is %s should be '%s'", resp.Secret, expectedSecret)
 	}
 }
