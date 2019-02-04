788ddc0a297e394cd0615d479f4bdfc09fa60627
Johan Haals <johan.haals@gmail.com>
2019-02-04 20:14:10 +0100

    exit on missing go files

--- .github/action-deploy/entrypoint.sh
+++ .github/action-deploy/entrypoint.sh
@@ -5,11 +5,15 @@ if [ "$GITHUB_REF" != "refs/heads/master" ]; then
     exit 0
 fi
 
-git diff-tree --no-commit-id --name-only -r HEAD | \
-    grep -E '\.go|aws-lambda' || echo 'No changes to go files, exiting' && exit 0
+if git diff-tree --no-commit-id --name-only -r HEAD | \
+    grep -E '\.go|aws-lambda'; then
+    cd deploy/aws-lambda || (echo 'Failed to cd to deploy/aws-lambda' && exit 1)
+    echo 'Building lamba server'
+    go build -o main
+    echo 'Deploying'
+    serverless deploy
+else
+    echo 'No changes to go files, exiting'
+    exit 0
+fi
 
-cd deploy/aws-lambda || (echo 'Failed to cd to deploy/aws-lambda' && exit 1)
-echo 'Building lamba server'
-go build -o main
-echo 'Deploying'
-serverless deploy
