9103e59820a8b88e4576c8f0b9a10f6ec8471ac7
Johan Haals <johan.haals@gmail.com>
2019-06-25 13:06:10 +0200

    Prevent out of range character selection

--- website/src/Create.tsx
+++ website/src/Create.tsx
@@ -155,7 +155,7 @@ const randomInt = (min: number, max: number): number => {
   const byteArray = new Uint8Array(1);
   window.crypto.getRandomValues(byteArray);
 
-  const range = max - min + 1;
+  const range = max - min;
   const maxRange = 256;
   if (byteArray[0] >= Math.floor(maxRange / range) * range) {
     return randomInt(min, max);
