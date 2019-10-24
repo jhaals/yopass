9344a6721ba4a7c89ee3329e369c3ceeefb45955
Christopher Hlubek <hlubek@networkteam.com>
2019-10-24 15:09:56 +0200

    Implement Redis database

--- cmd/yopass/main.go
+++ cmd/yopass/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/jhaals/yopass/pkg/yopass"
@@ -15,7 +16,8 @@ import (
 var (
 	address   = pflag.String("address", "", "listen address (default 0.0.0.0)")
 	maxLength = pflag.Int("max-length", 10000, "max length of encrypted secret")
-	memcached = pflag.String("memcached", "localhost:11211", "memcached address")
+	memcached = pflag.String("memcached", "", "Memcached address (e.g. localhost:11211)")
+	redis     = pflag.String("redis", "", "Redis URL (e.g. redis://localhost:6379/0)")
 	port      = pflag.Int("port", 1337, "listen port")
 	tlsCert   = pflag.String("tls-cert", "", "path to TLS certificate")
 	tlsKey    = pflag.String("tls-key", "", "path to TLS key")
@@ -28,10 +30,31 @@ func main() {
 	viper.BindPFlags(pflag.CommandLine)
 
 	addr := fmt.Sprintf("%s:%d", viper.GetString("address"), viper.GetInt("port"))
-	memcache := viper.GetString("memcached")
-	log.Printf("Starting yopass on %s, configured memcached address: %s", addr, memcache)
+	memcached := viper.GetString("memcached")
+	redis := viper.GetString("redis")
 
-	y := yopass.New(yopass.NewMemcached(memcache), viper.GetInt("max-length"))
+	var (
+		db yopass.Database
+		dbLog string
+		err error
+	)
+	if memcached != "" {
+		db = yopass.NewMemcached(memcached)
+		dbLog = fmt.Sprintf("configured Memcached address: %s", memcached)
+	} else if redis != "" {
+		db, err = yopass.NewRedis(redis)
+		if err != nil {
+			log.Fatalf("invalid Redis URL: %v", err)
+		}
+		dbLog = fmt.Sprintf("configured Redis URL: %s", redis)
+	} else {
+		fmt.Fprintf(os.Stderr, "Either Memcached address or Redis URL must be specified:\n")
+		pflag.Usage()
+		os.Exit(1)
+	}
+
+	log.Printf("Starting yopass on %s, %s", addr, dbLog)
+	y := yopass.New(db, viper.GetInt("max-length"))
 	if viper.GetString("tls-cert") != "" && viper.GetString("tls-key") != "" {
 		server := &http.Server{
 			Addr:      addr,
--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/jhaals/yopass
 
 require (
 	github.com/bradfitz/gomemcache v0.0.0-20180710155616-bc664df96737
+	github.com/go-redis/redis/v7 v7.0.0-beta.4
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	github.com/satori/go.uuid v1.2.0
