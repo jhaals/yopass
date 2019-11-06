05241cc07041ebebdf83157d1ab10d62bb2503f7
Johan Haals <johan.haals@gmail.com>
2019-11-06 22:23:45 +0100

    Reuse handlers for files and secrets

--- pkg/yopass/yopass.go
+++ pkg/yopass/yopass.go
@@ -27,8 +27,6 @@ func New(db Database, maxLength int) Yopass {
 
 var secret struct {
 	Expiration int32  `json:"expiration,omitempty"`
-	File       string `json:"file,omitempty"`
-	FileName   string `json:"file_name,omitempty"`
 	Message    string `json:"secret,omitempty"`
 }
 
@@ -62,83 +60,6 @@ func (y *Yopass) createSecret(w http.ResponseWriter, request *http.Request) {
 	w.Write(jsonData)
 }
 
-func (y *Yopass) storeFile(w http.ResponseWriter, request *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	decoder := json.NewDecoder(request.Body)
-	if err := decoder.Decode(&secret); err != nil {
-		http.Error(w, `{"message": "Unable to parse json"}`, http.StatusBadRequest)
-		return
-	}
-
-	if secret.FileName == "" || secret.File == "" {
-		http.Error(w, `{"message": "Fields missing"}`, http.StatusBadRequest)
-		return
-	}
-
-	if !validExpiration(secret.Expiration) {
-		http.Error(w, `{"message": "Invalid expiration specified"}`, http.StatusBadRequest)
-		return
-	}
-
-	if len(secret.File) > y.maxLength {
-		http.Error(w, `{"message": "File is too large"}`, http.StatusBadRequest)
-		return
-	}
-
-	if len(secret.FileName) > 1024 {
-		http.Error(w, `{"message": "File name is too long"}`, http.StatusBadRequest)
-		return
-	}
-
-	// Generate new UUID and store secret in memcache with specified expiration
-	key := uuid.NewV4().String()
-	if err := y.db.Put(key, secret.File, secret.Expiration); err != nil {
-		http.Error(w, `{"message": "Failed to store file in database"}`, http.StatusInternalServerError)
-		return
-	}
-
-	if err := y.db.Put(key+"name", secret.FileName, secret.Expiration); err != nil {
-		http.Error(w, `{"message": "Failed to store file in database"}`, http.StatusInternalServerError)
-		return
-	}
-	resp := map[string]string{"message": key}
-	jsonData, _ := json.Marshal(resp)
-	w.Write(jsonData)
-}
-
-func (y *Yopass) getFile(w http.ResponseWriter, request *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	key := mux.Vars(request)["key"]
-
-	secret, err := y.db.Get(key)
-	if err != nil {
-		http.Error(w, `{"message": "Secret not found"}`, http.StatusNotFound)
-		return
-	}
-
-	if err := y.db.Delete(key); err != nil {
-		http.Error(w, `{"message": "Failed to clear secret"}`, http.StatusInternalServerError)
-		return
-	}
-
-	fileName, err := y.db.Get(key + "name")
-	if err != nil {
-		http.Error(w, `{"message": "Secret not found"}`, http.StatusNotFound)
-		return
-	}
-
-	if err := y.db.Delete(key + "name"); err != nil {
-		http.Error(w, `{"message": "Failed to clear secret"}`, http.StatusInternalServerError)
-		return
-	}
-
-	resp, _ := json.Marshal(map[string]string{
-		"file":      secret,
-		"file_name": fileName,
-	})
-	w.Write(resp)
-}
-
 // getSecret from database
 func (y *Yopass) getSecret(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -152,7 +73,7 @@ func (y *Yopass) getSecret(w http.ResponseWriter, request *http.Request) {
 		http.Error(w, `{"message": "Failed to clear secret"}`, http.StatusInternalServerError)
 		return
 	}
-	resp, _ := json.Marshal(map[string]string{"message": string(secret)})
+	resp, _ := json.Marshal(map[string]string{"message": secret})
 	w.Write(resp)
 }
 
@@ -162,8 +83,8 @@ func (y *Yopass) HTTPHandler() http.Handler {
 	mx.HandleFunc("/secret/{key:(?:[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12})}",
 		y.getSecret)
 	mx.HandleFunc("/secret", y.createSecret).Methods("POST")
-	mx.HandleFunc("/file", y.storeFile).Methods("POST")
-	mx.HandleFunc("/file/{key:(?:[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12})}", y.getFile)
+	mx.HandleFunc("/file", y.createSecret).Methods("POST")
+	mx.HandleFunc("/file/{key:(?:[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12})}", y.getSecret)
 	mx.PathPrefix("/").Handler(http.FileServer(http.Dir("public")))
 	return handlers.LoggingHandler(os.Stdout, mx)
 }
