5a1b853b00608cff4c980475da140248e1477f6f
David Durieux <david@durieux.family>
2020-10-16 13:39:26 +0200

    Manage detection of the lang based on the browser lang

--- website/src/i18n.tsx
+++ website/src/i18n.tsx
@@ -1,35 +1,38 @@
 import i18n from "i18next";
 import { initReactI18next } from 'react-i18next';
-
+import detector from "i18next-browser-languagedetector";
 
 import Backend from 'i18next-http-backend';
 
 i18n
-  .use(initReactI18next)
+  .use(detector)
   .use(Backend)
+  .use(initReactI18next)
   .init({
+    detection: {order: ['navigator', 'querystring']},
+    backend: {
+      loadPath: '/yopass/locales/{{lng}}.json'
+    },
 
-  backend: {
-    loadPath: '/locales/{{lng}}.json'
-  },
+    fallbackLng: "en",
+    debug: true,
 
-  fallbackLng: "en",
-  lng: "en",
-  debug: false,
+    // have a common namespace used around the full app
+    ns: ["translations"],
+    defaultNS: "translations",
 
-  // have a common namespace used around the full app
-  ns: ["translations"],
-  defaultNS: "translations",
+    keySeparator: false, // we use content as keys
 
-  keySeparator: false, // we use content as keys
+    interpolation: {
+      escapeValue: false, // not needed for react!!
+      formatSeparator: ","
+    },
 
-  interpolation: {
-    escapeValue: false, // not needed for react!!
-    formatSeparator: ","
-  },
+    appendNamespaceToMissingKey: true,
 
-  appendNamespaceToMissingKey: true,
-
+    react: {
+      wait: true
+    }
 });
 
 export default i18n;
