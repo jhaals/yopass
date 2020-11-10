31462315737cc98a4d228b7147df09cd89520287
Johan Haals <johan.haals@gmail.com>
2020-11-10 09:22:45 +0100

    format

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Suspense } from "react";
-import App from "./App";
-import "./i18n";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Suspense } from 'react';
+import App from './App';
+import './i18n';
 
 ReactDOM.render(
   <Suspense fallback={<div>Loading...</div>}>
     <App />
   </Suspense>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
