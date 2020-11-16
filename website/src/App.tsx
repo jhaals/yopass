8e808ca2e4f419c5fa852f1bdfceb4455b08b60e
Andrew Cheremisov <git@acheremisov.com>
2020-11-15 19:38:47 -0500

    Removing features, docker build

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
       <Container className="margin">
         <Routes />
       </Container>
-      <Features />
+      {/* <Features /> */}
       <Attribution />
     </Router>
   );
