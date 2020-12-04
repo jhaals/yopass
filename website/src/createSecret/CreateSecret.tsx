27f9792f5e40122c3d6d0576942d8c9d3ac41be7
Johan Haals <johan.haals@gmail.com>
2020-12-04 21:27:57 +0100

    Move Lifetime to separate file

--- website/src/createSecret/CreateSecret.tsx
+++ website/src/createSecret/CreateSecret.tsx
@@ -1,17 +1,10 @@
 import { useTranslation } from 'react-i18next';
-import {
-  Alert,
-  Button,
-  Form,
-  FormGroup,
-  FormText,
-  Input,
-  Label,
-} from 'reactstrap';
+import { Alert, Button, Form, FormGroup, Input, Label } from 'reactstrap';
 import { useForm, UseFormMethods } from 'react-hook-form';
 import randomString, { encryptMessage, postSecret } from '../utils/utils';
 import React, { useState } from 'react';
 import Result from '../displaySecret/Result';
+import Lifetime from './Lifetime';
 
 const CreateSecret: React.FC = () => {
   const { t } = useTranslation();
@@ -180,55 +173,6 @@ export const SpecifyPasswordToggle: React.FC<{
       >
         {t('Generate decryption key')}
       </Label>
-    </FormGroup>
-  );
-};
-
-export const Lifetime: React.FC<{
-  register: UseFormMethods['register'];
-}> = (props) => {
-  const { t } = useTranslation();
-
-  const buttons = [];
-  for (const i of [
-    {
-      duration: 3600,
-      name: '1h',
-      text: t('One Hour'),
-    },
-    {
-      duration: 86400,
-      name: '1d',
-      text: t('One Day'),
-    },
-    {
-      duration: 604800,
-      name: '1w',
-      text: t('One Week'),
-    },
-  ]) {
-    buttons.push(
-      <FormGroup key={i.name} check={true} inline={true}>
-        <Label check={true}>
-          <Input
-            type="radio"
-            name="lifetime"
-            defaultChecked={i.duration === 3600}
-            value={i.duration}
-            innerRef={props.register({ required: true })}
-          />
-          {i.text}
-        </Label>
-      </FormGroup>,
-    );
-  }
-
-  return (
-    <FormGroup tag="fieldset">
-      <FormText color="muted">
-        {t('The encrypted message will be deleted automatically after')}
-      </FormText>
-      {buttons}
     </FormGroup>
   );
 };
