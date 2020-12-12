8e3dae5ad67f5dcf92b7f3116fe308fcfa85b994
jesperorb <jesperorb@gmail.com>
2020-12-12 14:42:23 +0100

    Convert: Header

--- website/src/shared/Header.tsx
+++ website/src/shared/Header.tsx
@@ -1,14 +1,35 @@
-import { Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
+import { AppBar, Toolbar, Typography, Button, Box, makeStyles } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    marginBottom: theme.spacing(4)
+  }
+}));
 
 export const Header = () => {
+  const classes = useStyles();
   return (
-    <Navbar color="dark" dark={true} expand="md">
-      <NavbarBrand href="/">
-        Yopass <img width="40" height="40" alt="" src="yopass.svg" />
-      </NavbarBrand>
-      <NavItem>
-        <NavLink href="/#/upload">Upload</NavLink>
-      </NavItem>
-    </Navbar>
+    <AppBar position="static" className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6" component="div">
+          Yopass <img width="40" height="40" alt="" src="yopass.svg" />
+        </Typography>
+        <Box
+          sx={{
+            marginLeft: 'auto',
+          }}
+        >
+          <Button
+            component={RouterLink}
+            to="/#/upload"
+            variant="contained"
+            color="secondary"
+          >
+            Upload
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
