a8948fd178db68c557082036965122a433d54f46
Johan Haals <johan.haals@gmail.com>
2020-12-15 13:31:38 +0100

    Make header clickable

--- website/src/shared/Header.tsx
+++ website/src/shared/Header.tsx
@@ -5,12 +5,21 @@ import {
   Button,
   Box,
   makeStyles,
+  Link,
 } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     marginBottom: theme.spacing(4),
+  },
+  header: {
+    color: 'white',
+    textDecoration: 'none',
+    '&:hover': {
+      color: 'white',
+      textDecoration: 'none',
+    },
   },
 }));
 
@@ -20,7 +29,9 @@ export const Header = () => {
     <AppBar position="static" className={classes.appBar}>
       <Toolbar>
         <Typography variant="h6" component="div">
-          Yopass <img width="40" height="40" alt="" src="yopass.svg" />
+          <Link href="/#/" className={classes.header}>
+            Yopass <img width="40" height="40" alt="" src="yopass.svg" />
+          </Link>
         </Typography>
         <Box
           sx={{
