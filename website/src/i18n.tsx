b312eb3b0986c2a8e83c17e605c310aea80fba17
Sebastien CLAUDEL <sebastien.claudel@darva.com>
2021-06-24 13:40:14 +0200

    Adding french translation

--- website/src/i18n.tsx
+++ website/src/i18n.tsx
@@ -1,23 +1,26 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
 
 import Backend from 'i18next-http-backend';
 
 i18n
   .use(initReactI18next)
+  .use(LanguageDetector)
   .use(Backend)
   .init({
-    backend: {
-      loadPath: process.env.PUBLIC_URL + '/locales/{{lng}}.json',
-    },
-
     fallbackLng: 'en',
-    lng: 'en',
     debug: false,
-
+    detection:{
+      order: ['navigator']
+    },
     // have a common namespace used around the full app
     ns: ['translations'],
     defaultNS: 'translations',
+    backend: {
+      loadPath: process.env.PUBLIC_URL + '/locales/{{lng}}.json',
+    },
 
     keySeparator: false, // we use content as keys
 
