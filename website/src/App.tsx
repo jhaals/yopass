8f36303ee3af1ad1fdd1c1c5b2d8137884c7a0d9
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@elvia.no>
2021-08-18 14:37:08 +0200

    core-910: revert back to hash router

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
 
