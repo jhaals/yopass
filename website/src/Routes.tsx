e0a11c6649551b37ee2bc584a2d3687a41fe0bfd
Warnar Boekkooi <warnar.boekkooi@lengoo.de>
2021-08-25 16:07:26 +0200

    Add Click through to Secret Path
    In order to avoid previews from messing and removing secrets we provide a click through page.
    This page will not check if the secret exists or do any fetching whatsoever. This hopefully fixes the issues described in https://github.com/jhaals/yopass/issues/741.

--- website/src/Routes.tsx
+++ website/src/Routes.tsx
@@ -1,12 +1,13 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import CreateSecret from './createSecret/CreateSecret';
 import DisplaySecret from './displaySecret/DisplaySecret';
 import Upload from './createSecret/Upload';
+import ClickThroughToSecret from './displaySecret/ClickThroughToSecret';
 
 export const Routes = () => {
   return (
-    <div>
+    <Switch>
       <Route path="/" exact={true} component={CreateSecret} />
       <Route path="/upload" exact={true} component={Upload} />
       <Route
@@ -14,6 +15,11 @@ export const Routes = () => {
         path="/:format(s|f)/:key/:password?"
         component={DisplaySecret}
       />
-    </div>
+      <Route
+        exact={true}
+        path="/c/:format(s|f)/:key/:password?"
+        component={ClickThroughToSecret}
+      />
+    </Switch>
   );
 };
---
+++ website/src/displaySecret/ClickThroughToSecret.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+import { generatePath, useHistory, useParams } from 'react-router-dom';
+import {
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+
+const ClickThroughToSecret = () => {
+  const { format, key, password: paramsPassword } = useParams<{
+    format: string;
+    key: string;
+    password: string;
+  }>();
+  const isFile = format === 'f';
+  const { t } = useTranslation();
+  const history = useHistory();
+
+  const [password, setPassword] = useState('');
+  const [invalidPassword, setInvalidPassword] = useState(false);
+
+  // The state can go out of sync when using back buttons
+  // So to ensure that password is using the correct version set the url password when it exists
+  if (!password && paramsPassword) {
+    setPassword(paramsPassword);
+  }
+
+  const toSecret = () => {
+    if (!password) {
+      setInvalidPassword(true);
+      return;
+    }
+    const pathname = generatePath('/:format(s|f)/:key/:password', {
+      format: format,
+      key: key,
+      password: password,
+    });
+
+    history.push({ pathname: pathname, state: {} });
+  };
+
+  if (paramsPassword) {
+    return (
+      <Container maxWidth="lg">
+        <Grid container direction="column" spacing={1}>
+          <Grid item xs={12}>
+            <Button variant="contained" onClick={toSecret}>
+              {isFile
+                ? t('click-through.buttonSecret')
+                : t('click-through.buttonFile')}
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    );
+  }
+
+  return (
+    <Container maxWidth="lg">
+      <Grid container direction="column" spacing={1}>
+        <Grid item xs={12}>
+          <Typography variant="h5">Enter decryption key</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            autoFocus
+            name="decryptionKey"
+            id="decryptionKey"
+            placeholder={t('display.inputDecryptionKeyPlaceholder')}
+            label={t('display.inputDecryptionKeyLabel')}
+            value={password}
+            error={invalidPassword}
+            helperText={invalidPassword && t('display.errorInvalidPassword')}
+            onChange={(e) => setPassword(e.target.value)}
+            inputProps={{ spellCheck: 'false', 'data-gramm': 'false' }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" onClick={toSecret}>
+            {isFile
+              ? t('click-through.buttonSecret')
+              : t('click-through.buttonFile')}
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default ClickThroughToSecret;
