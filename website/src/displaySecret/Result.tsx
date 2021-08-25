c748dbedcc4fd1cc191d70a8067d1798c01c0c7d
Warnar Boekkooi <warnar.boekkooi@lengoo.de>
2021-08-25 16:31:11 +0200

    Add click through page links

--- website/src/displaySecret/Result.tsx
+++ website/src/displaySecret/Result.tsx
@@ -23,27 +23,49 @@ type ResultProps = {
 const Result = ({ uuid, password, prefix, customPassword }: ResultProps) => {
   const base =
     (process.env.PUBLIC_URL ||
-      `${window.location.protocol}//${window.location.host}`) + `/#/${prefix}`;
-  const short = `${base}/${uuid}`;
+      `${window.location.protocol}//${window.location.host}`) + `/#`;
+
+  const short = `${base}/${prefix}/${uuid}`;
   const full = `${short}/${password}`;
+  const clickThroughShort = `${base}/c/${prefix}/${uuid}`;
+  const clickThroughFull = `${clickThroughShort}/${password}`;
   const { t } = useTranslation();
 
   return (
     <Box>
       <Typography variant="h4">{t('result.title')}</Typography>
-      <Typography>
-        {t('result.subtitleDownloadOnce')}
-        <br />
-        {t('result.subtitleChannel')}
-      </Typography>
       <TableContainer>
         <Table>
           <TableBody>
+            <TableRow>
+              <TableCell colSpan={3}>
+                <Typography>
+                  {t('result.subtitleDownloadOnce')}
+                  <br />
+                  {t('result.subtitleChannel')}
+                </Typography>
+              </TableCell>
+            </TableRow>
             {!customPassword && (
               <Row label={t('result.rowLabelOneClick')} value={full} />
             )}
             <Row label={t('result.rowLabelShortLink')} value={short} />
             <Row label={t('result.rowLabelDecryptionKey')} value={password} />
+            <TableRow>
+              <TableCell colSpan={3}>
+                <Typography>{t('result.subtitleClickThrough')}</Typography>
+              </TableCell>
+            </TableRow>
+            {!customPassword && (
+              <Row
+                label={t('result.rowLabelClickThroughOneClick')}
+                value={clickThroughFull}
+              />
+            )}
+            <Row
+              label={t('result.rowLabelClickThroughShortLink')}
+              value={clickThroughShort}
+            />
           </TableBody>
         </Table>
       </TableContainer>
