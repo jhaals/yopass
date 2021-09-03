f3a0687a1b69863e03daebda6d8a94768d0772cc
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@elvia.no>
2021-09-03 12:17:17 +0200

    fix: core-910 yarn run format & lint

--- website/cypress/integration/load_website.js
+++ website/cypress/integration/load_website.js
@@ -1,23 +1,30 @@
-describe('Correct Home Page', () => {
-  it('Successfully Loads', () => {
+describe('Sign-In Button', () => {
+  it('should enable', () => {
     cy.visit('http://localhost:3000/#/');
     cy.contains('button', 'Sign-In').should('be.enabled');
   });
 });
 
-describe('Correct Blank Page Description', () => {
-  it('Successfully Loads', () => {
+describe('Blank Page Description', () => {
+  it('should display', () => {
     cy.visit('http://localhost:3000/#/');
     cy.get('#headerDescription').should('have.text', 'Share Secrets Securely');
   });
 });
 
-describe('Correct Header Description', () => {
-  it('Successfully Loads', () => {
+describe('Header Description', () => {
+  it('should display', () => {
     cy.visit('http://localhost:3000/#/');
     cy.get('#blankPageDescription').should(
       'have.text',
       'This page intentionally left blank.',
     );
+  });
+});
+
+describe('Correct Header Icon', () => {
+  it('Successfully Loads', () => {
+    cy.visit('http://localhost:3000/#/');
+    cy.get('#headerIconImage').should('be.visible');
   });
 });
