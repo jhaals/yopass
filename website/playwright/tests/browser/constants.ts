4b7089646bef223d347b16ded02ccf8b6b84eeba
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@elvia.no>
2021-09-20 11:03:47 +0200

    core-1036: centralize contants

---
+++ website/playwright/tests/browser/constants.ts
@@ -0,0 +1,8 @@
+import path from 'path';
+
+export const cookiesFileName = 'cookies.json';
+export const cookiesFilePath = process.cwd() + path.sep + cookiesFileName;
+export const storageStateFileName = 'storage_state.json';
+export const storageStateFilePath =
+  process.cwd() + path.sep + storageStateFileName;
+export const testUserEmail = 'onetime.testuser@internal.testuser';
