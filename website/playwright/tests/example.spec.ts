28539076123ea675bf43cffea0c953e3743790d8
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@elvia.no>
2021-09-20 23:12:36 +0200

    core-1036: delete redundant tests

--- website/playwright/tests/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('example', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
-  await page.screenshot({ path: 'tests/output/example.png' });
-});
