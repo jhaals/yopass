84992f436802b7c43f82a4a099c40700f6e17bdd
Warnar Boekkooi <warnar.boekkooi@lengoo.de>
2021-09-29 14:47:43 +0200

    Use local Roboto font
    In order to avoid external calls from YoPass we now embed the roboto font using the [`@fontsource/roboto`](https://fontsource.org/fonts/roboto) package.
    This should resolve https://github.com/jhaals/yopass/issues/960.

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -2,6 +2,10 @@ import ReactDOM from 'react-dom';
 import { Suspense } from 'react';
 import App from './App';
 import './i18n';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
   <Suspense fallback={<div>Loading...</div>}>
