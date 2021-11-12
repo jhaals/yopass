8c76a4a0faee824f07058f370c4be492cfa74d32
Warnar Boekkooi <warnar.boekkooi@lengoo.de>
2021-11-12 14:10:27 +0100

    Remove username
    The username information is not really adding any value so let's remove it.

--- pkg/server/logging.go
+++ pkg/server/logging.go
@@ -15,13 +15,6 @@ func httpLogFormatter(logger *zap.Logger) func(io.Writer, handlers.LogFormatterP
 	return func(_ io.Writer, params handlers.LogFormatterParams) {
 		var req = params.Request
 
-		username := "-"
-		if params.URL.User != nil {
-			if name := params.URL.User.Username(); name != "" {
-				username = name
-			}
-		}
-
 		host, _, err := net.SplitHostPort(req.RemoteAddr)
 		if err != nil {
 			host = req.RemoteAddr
@@ -42,7 +35,6 @@ func httpLogFormatter(logger *zap.Logger) func(io.Writer, handlers.LogFormatterP
 		logger.Info(
 			"Request handled",
 			zap.String("host", host),
-			zap.String("username", username),
 			zap.Time("timestamp", params.TimeStamp),
 			zap.String("method", req.Method),
 			zap.String("uri", uri),
