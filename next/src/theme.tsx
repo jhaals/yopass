9e5f3e0137d01890aadca37ace6f58eae1b12eb8
Johan Haals <johan.haals@gmail.com>
2021-12-10 11:31:21 +0100

    fix theme
    Signed-off-by: Johan Haals <johan.haals@gmail.com>

--- next/src/theme.tsx
+++ next/src/theme.tsx
@@ -1,18 +1,9 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { createTheme } from "@mui/material/styles";
+import { blueGrey } from "@mui/material/colors";
 
-// Create a theme instance.
 const theme = createTheme({
   palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
+    primary: blueGrey,
   },
 });
 
