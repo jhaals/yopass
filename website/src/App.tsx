b8849ae9a454a09c6bf1d0722a18c945b1e24173
Johan Haals <johan.haals@gmail.com>
2021-12-10 13:30:58 +0100

    Remove makeStyles usages
    Signed-off-by: Johan Haals <johan.haals@gmail.com>

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,9 +1,5 @@
 import { Container } from '@mui/material';
-import {
-  ThemeProvider,
-  Theme,
-  StyledEngineProvider,
-} from '@mui/material/styles';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 
 import { Header } from './shared/Header';
 import { Routing } from './Routing';
@@ -11,11 +7,6 @@ import { Features } from './shared/Features';
 import { Attribution } from './shared/Attribution';
 import { theme } from './theme';
 import { HashRouter } from 'react-router-dom';
-
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
 
 const App = () => {
   // TODO: Removed in future version.
