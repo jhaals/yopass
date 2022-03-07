56b95d4f65630627bf61e05839f591b1d1354949
Johan Haals <johan.haals@gmail.com>
2022-03-07 11:47:51 +0100

    next: include font, configure csp
    Signed-off-by: Johan Haals <johan.haals@gmail.com>

--- next/next.config.js
+++ next/next.config.js
@@ -1,9 +1,21 @@
 const { i18n } = require('./next-i18next.config');
+const nextSafe = require('next-safe');
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   i18n,
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: nextSafe({ isDev }),
+      },
+    ];
   },
 };
