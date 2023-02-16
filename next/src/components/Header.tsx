060c822794805c29b2235856170cf2fba9b3a9fd
Ruben Das <ruben.das@tink.se>
2023-02-16 15:03:50 +0100

    refactor(website): remove upload option
    Upload feature is deemed as hazardous security wise.

--- next/src/components/Header.tsx
+++ next/src/components/Header.tsx
@@ -1,14 +1,12 @@
-import { AppBar, Toolbar, Typography, Button, Box, Link } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, Link } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useRouter } from 'next/router';
 
 export const Header = () => {
   const { t } = useTranslation();
   const router = useRouter();
-  const isOnUploadPage = router.pathname.includes('upload');
   const base = process.env.PUBLIC_URL || '';
   const home = base + '/';
-  const upload = base + '/upload';
   return (
     <AppBar position="static" color="transparent" sx={{ marginBottom: 4 }}>
       <Toolbar>
@@ -34,14 +32,6 @@ export const Header = () => {
             marginLeft: 'auto',
           }}
         >
-          <Button
-            component={Link}
-            href={isOnUploadPage ? home : upload}
-            variant="contained"
-            color="primary"
-          >
-            {isOnUploadPage ? t('header.buttonHome') : t('header.buttonUpload')}
-          </Button>
         </Box>
       </Toolbar>
     </AppBar>
--- website/src/shared/Header.tsx
+++ website/src/shared/Header.tsx
@@ -1,14 +1,8 @@
-import { AppBar, Toolbar, Typography, Button, Box, Link } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import { useLocation } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Box, Link } from '@mui/material';
 
 export const Header = () => {
-  const { t } = useTranslation();
-  const location = useLocation();
-  const isOnUploadPage = location.pathname.includes('upload');
   const base = process.env.PUBLIC_URL || '';
   const home = base + '/#/';
-  const upload = base + '/#/upload';
   return (
     <AppBar position="static" color="transparent" sx={{ marginBottom: 4 }}>
       <Toolbar>
@@ -34,14 +28,6 @@ export const Header = () => {
             marginLeft: 'auto',
           }}
         >
-          <Button
-            component={Link}
-            href={isOnUploadPage ? home : upload}
-            variant="contained"
-            color="primary"
-          >
-            {isOnUploadPage ? t('header.buttonHome') : t('header.buttonUpload')}
-          </Button>
         </Box>
       </Toolbar>
     </AppBar>
