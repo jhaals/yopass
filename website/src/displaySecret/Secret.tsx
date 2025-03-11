3fa70fe58a8236360328f5ec675284928bc035ff
Dennis Greguhn <dev@greguhn.de>
2025-03-11 18:47:53 +0100

    Add button to show/hide QR code

--- website/src/displaySecret/Secret.tsx
+++ website/src/displaySecret/Secret.tsx
@@ -4,12 +4,13 @@ import { faCopy } from '@fortawesome/free-solid-svg-icons';
 import { Box, Button, Typography } from '@mui/material';
 import { useCopyToClipboard } from 'react-use';
 import { saveAs } from 'file-saver';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import QRCode from 'react-qr-code';
 
 const RenderSecret = ({ secret }: { readonly secret: string }) => {
   const { t } = useTranslation();
   const [copy, copyToClipboard] = useCopyToClipboard();
+  const [showQr, setShowQr] = useState(false);
 
   return (
     <div>
@@ -39,7 +40,10 @@ const RenderSecret = ({ secret }: { readonly secret: string }) => {
       >
         {secret}
       </Typography>
-      <Box sx={{ display:"flex", justifyContent:"center", alignItems:"center", margin:5 }}>
+	  <Button
+	  	onClick={() => setShowQr(!showQr)}
+	  >Show QR Code</Button>
+      <Box sx={{ display:(showQr ? "flex" : "none"), justifyContent:"center", alignItems:"center", margin:5 }}>
         <QRCode
           size={512}
           style={{ height: "auto" }}
