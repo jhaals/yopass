1151bc9e65fc318da514ff5744a8189835e898a5
Johan Haals <johan.haals@gmail.com>
2025-08-28 22:08:24 +0200

    Enhance Upload component icon styling and SVG structure
    - Updated the SVG icon in the Upload component to use stroke properties for better visual consistency.
    - Modified the path definition to improve the icon's appearance and responsiveness.
    
    Signed-off-by: Johan Haals <johan.haals@gmail.com>

--- next/src/Upload.tsx
+++ next/src/Upload.tsx
@@ -140,11 +140,17 @@ function Upload() {
             <div className="flex flex-col items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
+                fill="none"
                 viewBox="0 0 24 24"
-                fill="currentColor"
+                strokeWidth={1.5}
+                stroke="currentColor"
                 className="w-14 h-14 text-base-content/60"
               >
-                <path d="M12 16v-8m0 0l-3 3m3-3l3 3m-9 4v2a2 2 0 002 2h8a2 2 0 002-2v-2" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
+                />
               </svg>
               <div className="mt-2 font-semibold">
                 {file ? file.name : "Drag & drop or click to choose a file"}
