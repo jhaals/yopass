14933daf3057c039c8b53e2d866fdd47f8e20718
Anturix <git@anturix.de>
2025-09-06 23:15:10 +0200

    ADD: add german language file

--- website/src/shared/locales/index.ts
+++ website/src/shared/locales/index.ts
@@ -5,3 +5,5 @@ export { default as sv } from './sv.json';
 // Add future language exports here
 // export { default as es } from './es.json';
 // export { default as fr } from './fr.json';
+
+export { default as de } from './de.json';
