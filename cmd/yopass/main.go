dc14c6abb35db1ab6007451c742a8623952c367f
AnwarParker <anwar.parker@impact.com>
2025-11-20 20:19:21 +0200

    FEATURE: Add additional expiration options

--- cmd/yopass/main.go
+++ cmd/yopass/main.go
@@ -69,7 +69,7 @@ func init() {
 	pflag.CommandLine = pflag.NewFlagSet(os.Args[0], pflag.ContinueOnError)
 	pflag.String("api", viper.GetString("api"), "Yopass API server location")
 	pflag.String("decrypt", viper.GetString("decrypt"), "Decrypt secret URL")
-	pflag.String("expiration", viper.GetString("expiration"), "Duration after which secret will be deleted [1h, 1d, 1w]")
+	pflag.String("expiration", viper.GetString("expiration"), "Duration after which secret will be deleted [1h, 1d, 1w, 2w, 1m]")
 	pflag.String("file", viper.GetString("file"), "Read secret from file instead of stdin")
 	pflag.String("key", viper.GetString("key"), "Manual encryption/decryption key")
 	pflag.Bool("one-time", viper.GetBool("one-time"), "One-time download")
@@ -204,6 +204,10 @@ func expiration(s string) int32 {
 		return 3600 * 24
 	case "1w":
 		return 3600 * 24 * 7
+	case "2w":
+		return 3600 * 24 * 14
+	case "1m":
+		return 3600 * 24 * 30
 	default:
 		return 0
 	}
