2a4fac464018261fe845819af2a729bcd7a3b490
Valentin PELUS <valentinpelus@La-pignolerie.local>
2026-03-03 22:11:31 +0900

    style(website/src): fix style for forced expiration message and add additionnal languages

--- website/src/shared/components/SecretOptions.tsx
+++ website/src/shared/components/SecretOptions.tsx
@@ -51,26 +51,11 @@ export function SecretOptions({
           </span>
         </label>
         {config?.FORCE_EXPIRATION && (
-          <div className="alert alert-info mb-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="stroke-current shrink-0 w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
-            <span>
-              {t('expiration.serverEnforcedMaximumExpiration', {
-                value: getExpirationLabel(config.FORCE_EXPIRATION),
-              })}
-            </span>
-          </div>
+          <p className="text-sm text-base-content/70 mb-2">
+            {t('expiration.serverEnforcedMessage', {
+              value: getExpirationLabel(config.FORCE_EXPIRATION),
+            })}
+          </p>
         )}
         <div className="flex flex-wrap gap-4 mt-2">
           {expirationOptions.map((option, index) => (
